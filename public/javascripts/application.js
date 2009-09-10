1fbcab0f0eb9cca4b094bec7f14f4ec4cc4edf3e
Darren Case <darren@darren-cases-macbook.local>
2009-09-10 15:10:59 -0400

    fixing hover in ie7

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -233,6 +233,7 @@ $(document).ready( function() {
 	$("#container .block, #container .content",".admin-mode").livequery( function() {
 		$(this).hover(
 			function() {
+				alert("over");
 				$(".active-admin").not($(this).parents()).removeClass("active-admin");
 				$(".admin-links:visible").hide();
 				$(this).addClass("active-admin");
