317ea669bf5595b3f3eff4c899c3dec7ad01e1c2
Darren Case <darren@darren-cases-macbook.local>
2009-09-16 12:43:47 -0400

    removed manager emails from job application mailer

--- app/models/job_application_mailer.rb
+++ app/models/job_application_mailer.rb
@@ -3,12 +3,7 @@ class JobApplicationMailer < ActionMailer::Base
 
   def submit(job_application)
     subject    'new online application from insightstrat.com'
-    recipients ['info@commandc.com', 
-      'betty.pio@insightstrat.com',
-      'cyrus.chowdhury@insightstrat.com',
-      'angela.kuo@insightstrat.com',
-      'darin.rubin@insightstrat.com',
-      'info@insightstrat.com']
+    recipients ['info@commandc.com','info@insightstrat.com']
     from       'apply_online@insightstrat.com'
     sent_on    Time.now
     
