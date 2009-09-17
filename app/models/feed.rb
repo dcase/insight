6490ab5b1164a13823787b29694545329b5021a3
Darren Case <darren@darren-cases-macbook.local>
2009-09-17 12:54:51 -0400

    still trying to fix update_all_feeds error - trying respond_to?

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -42,7 +42,7 @@ class Feed < ActiveRecord::Base
   def self.update_all_feeds
     Feed.all.each do |f|
       feed = Feedzirra::Feed.fetch_and_parse(f.feed_url)
-      unless defined?(feed.entries)
+      if feed.respond_to?('entries')
         f.add_entries(feed.entries, f.id) unless feed.entries.blank?
       end
     end
